5ab5f96d10a2a0ec4e145b260a4c554b0378c1c7
Stan <Stancool42@gmail.com>
2021-10-14 16:10:54 +0300

    1

--- src/com/pb/stetsuk/hw2/Calculator.java
+++ src/com/pb/stetsuk/hw2/Calculator.java
@@ -1,61 +1,43 @@
 package com.pb.stetsuk.hw2;
 import java.util.Scanner;
 
-public class Calculator {
-    static Scanner scanner = new Scanner(System.in);
-
+class Main {
     public static void main(String[] args) {
-        int x = getInt();
-        int y = getInt();
-        char operation = getOperation();
-        int result = calc(x, y, operation);
-        System.out.println("Результат: " + result);
-    }
-        public static int getInt() {
-            System.out.print("число: ");
-            int num;
-            if (scanner.hasNextInt()) {
-                num = scanner.nextInt();
-            } else {
-                System.out.println("Error");
-                scanner.next();
-                num = getInt();
-            }
-            return num;
-        }
+        char operator;
+        double number1, number2;
+        Scanner input = new Scanner(System.in);
 
-    public static char getOperation() {
-        System.out.print("Введите знак: ");
-        char operation;
-        if (scanner.hasNext()) {
-            operation = scanner.next().charAt(0);
-        } else {
-            System.out.println("Error");
-            scanner.next();
-            operation = getOperation();
-            }
-        return operation;
-    }
+        System.out.println("число 1");
+        number1 = input.nextDouble();
 
-    public static int calc (int num1, int num2, char operation) {
-        int y;
-        switch (operation) {
+        System.out.println("число 2");
+        number2 = input.nextDouble();
+
+        System.out.println("Выберите действие: ");
+        operator = input.next().charAt(0);
+
+        input.close();
+
+        switch (operator) {
             case '+':
-                y = num1 + num2;
+                System.out.printf("%f + %f = %f", number1, number2, (number1 + number2));
                 break;
             case '-':
-                y = num1 - num2;
+                System.out.printf("%f - %f = %f", number1, number2, (number1 - number2));
                 break;
             case '*':
-                y = num1 * num2;
+                System.out.printf("%f * %f = %f", number1, number2, (number1 * number2));
                 break;
             case '/':
-                y = num1 / num2;
+                if (number2 != 0)
+                System.out.printf("%f / %f = %f", number1, number2, (number1 / number2));
+                else
+                System.out.println("низя");
                 break;
+
             default:
-                System.out.println("Введите переменную");
-                y = calc(num1, num2, getOperation());
+                System.out.printf("%c Error");
         }
-        return y;
     }
 }
+
--- src/com/pb/stetsuk/hw2/Interval.java
+++ src/com/pb/stetsuk/hw2/Interval.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 public class Interval {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите переменную");
+        System.out.println("Введите переменную: ");
         int n = scanner.nextInt();
         System.out.println(n);
 
-        if (n >= 0 && n < 14) {
-            System.out.println("[0 -14]");
+        if (n >= 0 && n < 15) {
+            System.out.println("[0 - 14]");
         } else {
-            if (n >= 15 && n < 35) {
+            if (n >= 15 && n < 36) {
                 System.out.println("[15 - 35]");
             } else {
-                if (n > 36 && n <= 50) {
+                if (n >= 36 && n < 51) {
                     System.out.println("[36 - 50]");
                 } else {
-                    if (n > 51 && n <= 100) {
+                    if (n >= 51 && n <= 100) {
                         System.out.println("[51 - 100]");
                     } else {
                         if (n == 101) {
